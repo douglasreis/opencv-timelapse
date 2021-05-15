8350d3eb4642ce19aa4cc1ab2ffd47c235bd8f3e
DouglasReis <douglas.reis@totaljobsgroup.com>
2021-05-15 21:26:16 +0100

    Set default base path

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 config = {}
 config["runHour"] = 23
-config["basePath"] = "/srv/dev-disk-by-uuid-54F2-F066/Backup/timelapse"
+config["basePath"] = "/var/image"
 
 config["height"] = 1536
 config["width"] = 2048
